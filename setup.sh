fd5ad30e4e6856be7ebf40a97b94666b6fb2c6e6
Bakumov Vladimir <bakumov.vd@phystech.edu>
2020-11-11 09:07:04 +0300

    s

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 pip3 install -r requirements.txt
 chmod +x decoder.py encoder.py generate_keys.py
 mv decoder.py decoder
-mv emcoder.py encoder
+mv encoder.py encoder
 mv generate_keys.py generate_keys
